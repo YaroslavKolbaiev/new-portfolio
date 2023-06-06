28f14faf2081fb51529914856f2cad9e89c547f3
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-06-06 09:35:28 +0300

    fix: layout page

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
-export const Layout = ({ children }) => {
+export default function Layout({ children }) {
   return (
     <div className="layout">
       <Header />
