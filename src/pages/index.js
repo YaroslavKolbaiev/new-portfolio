65c5438f4d44e4be90c00e97ba0868f14e29859d
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-06-06 09:39:01 +0300

    fix: layout import path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import { Layout } from "@/pages/layout";
 import { Projects } from "@/components/Projects/Projects";
 import Hero from "@/components/Hero/Hero";
 import Head from "next/head";
+import Layout from "./layout";
 
 export default function Home() {
   return (
